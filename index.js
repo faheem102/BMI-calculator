6972933c739949e5e77fd94d3c7c0be43de1b6d8
Faheem Hassan <51878457+faheem102@users.noreply.github.com>
2023-11-09 23:02:49 +0100

    bmi calculation completed

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 console.log("hello world");
 
+function myFunction(){
+    let height = document.getElementById("cm-height").value/100;
+    let weight = document.getElementById("kilo-weight").value;
+    let bmi = Math.round(weight/(height*height));
+
+    const calcButton = document.getElementById("calc-btn");
+    const result = document.getElementById("results");
+    if(bmi >= 30){
+        result.innerHTML = "Your bmi is " + bmi + " you are obese";
+        
+    }
+    else if(bmi>18 && bmi < 25){
+        result.innerHTML = "Your bmi is " + bmi + " you are healthy";
+
+    }
+    else if(bmi<30 && bmi>25 ){
+        result.innerHTML = "Your bmi is " + bmi + " you are overweight";
+    }
+    else{
+        result.innerHTML = "Your bmi is " + bmi + " you are underweight";
+    }
+
+    
+}
 
