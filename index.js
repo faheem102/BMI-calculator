f53a092042b0bf16a37d6cb36bae26c47cb3fded
Faheem Hassan <51878457+faheem102@users.noreply.github.com>
2023-11-10 00:26:11 +0100

    changes to disallow negative input values

--- index.js
+++ index.js
@@ -7,19 +7,27 @@ function myFunction(){
 
     const calcButton = document.getElementById("calc-btn");
     const result = document.getElementById("results");
+    let measure;
     if(bmi >= 30){
-        result.innerHTML = "Your bmi is " + bmi + " you are obese";
+         measure = "Your bmi is " + bmi + " you are obese";
         
     }
     else if(bmi>18 && bmi < 25){
-        result.innerHTML = "Your bmi is " + bmi + " you are healthy";
+        measure = "Your bmi is " + bmi + " you are healthy";
 
     }
     else if(bmi<30 && bmi>25 ){
-        result.innerHTML = "Your bmi is " + bmi + " you are overweight";
+        measure = "Your bmi is " + bmi + " you are overweight";
     }
     else{
-        result.innerHTML = "Your bmi is " + bmi + " you are underweight";
+        measure = "Your bmi is " + bmi + " you are underweight";
+    }
+    if(weight<0 || height<0){
+        result.innerHTML = "You can enter negative values"
+
+    }
+    else{
+        result.innerHTML = measure;
     }
 
     
